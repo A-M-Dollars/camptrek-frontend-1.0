2bc03c3898a5dab891dc13ccdaceb3cf71c29dbd
A-M-Dollars <charity.k.mutembei@gmail.com>
2025-09-11 17:38:18 +0300

    fix chinese

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,19 @@ const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
 export const metadata: Metadata = {
   title: "Camptrek Safaris",
   description: `Discover the beauty of Africa with our unforgettable safari experiences. From thrilling game drives across the savannah to serene nature walks and luxury lodge stays, we connect you to the heart of the wild. Our tailored safari packages offer close encounters with the Big Five, breathtaking landscapes, and authentic cultural experiences. Whether you’re seeking adventure, relaxation, or a family-friendly journey, we make your dream safari a reality.`,
+  openGraph: {
+    title: "Camptrek Safaris",
+    description: `Discover the beauty of Africa with our unforgettable safari experiences. From thrilling game drives across the savannah to serene nature walks and luxury lodge stays, we connect you to the heart of the wild. Our tailored safari packages offer close encounters with the Big Five, breathtaking landscapes, and authentic cultural experiences. Whether you’re seeking adventure, relaxation, or a family-friendly journey, we make your dream safari a reality.`,
+    url: "https://www.camptreksafaris.com/",
+    siteName: "Camptrek Safaris",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Camptrek Safaris",
+    description: `Discover the beauty of Africa with our unforgettable safari experiences. From thrilling game drives across the savannah to serene nature walks and luxury lodge stays, we connect you to the heart of the wild. Our tailored safari packages offer close encounters with the Big Five, breathtaking landscapes, and authentic cultural experiences. Whether you’re seeking adventure, relaxation, or a family-friendly journey, we make your dream safari a reality.`,
+  },
 };
 
 export default function RootLayout({
