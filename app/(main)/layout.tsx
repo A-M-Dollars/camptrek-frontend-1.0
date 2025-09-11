be456b0e485aab9f0e18f8899356c685279a8b55
A-M-Dollars <charity.k.mutembei@gmail.com>
2025-09-11 17:27:22 +0300

    add description

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,17 +8,17 @@ type SafarisLayoutProps = {
 
 export const metadata: Metadata = {
     title: 'Camptrek Safaris - All Safaris',
-    description: 'Explore our range of safaris at Camptrek'
+    // description: `Discover the beauty of Africa with our unforgettable safari experiences. From thrilling game drives across the savannah to serene nature walks and luxury lodge stays, we connect you to the heart of the wild. Our tailored safari packages offer close encounters with the Big Five, breathtaking landscapes, and authentic cultural experiences. Whether you’re seeking adventure, relaxation, or a family-friendly journey, we make your dream safari a reality.`,
 }
 
-const safarisLayout = ({children}: SafarisLayoutProps) => {
-    return(
+const safarisLayout = ({ children }: SafarisLayoutProps) => {
+    return (
         <div>
             <Navbar />
             <div>
                 {children}
             </div>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const googleClientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
 
 export const metadata: Metadata = {
   title: "Camptrek Safaris",
-  description: "Explore the wild with Camptrek Safaris",
+  description: `Discover the beauty of Africa with our unforgettable safari experiences. From thrilling game drives across the savannah to serene nature walks and luxury lodge stays, we connect you to the heart of the wild. Our tailored safari packages offer close encounters with the Big Five, breathtaking landscapes, and authentic cultural experiences. Whether you’re seeking adventure, relaxation, or a family-friendly journey, we make your dream safari a reality.`,
 };
 
 export default function RootLayout({
