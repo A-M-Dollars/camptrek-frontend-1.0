01088ae11556e34946cf0f376ee6f93b85c39852
A-M-Dollars <charity.k.mutembei@gmail.com>
2025-09-15 10:12:11 +0300

    review

--- components/landing/hero-section/hero-section.tsx
+++ components/landing/hero-section/hero-section.tsx
@@ -14,7 +14,10 @@ function Herosection({ scrollToAbout }: HeroProps) {
             <div className="overflow-x-hidden relative w-screen ">
                 {/* Background Video */}
                 <video
-                    className="absolute brightness-60 top-0 left-0 min-w-full min-h-full object-cover -z-10"
+                    className="
+                    absolute brightness-60 top-0 left-0 min-w-full
+                    min-h-full lg:h-[50%] object-cover -z-10
+                    "
                     src="/hero/herovid.mp4"
                     autoPlay
                     muted
@@ -25,7 +28,7 @@ function Herosection({ scrollToAbout }: HeroProps) {
                 {/* Content on top */}
                 <div className="relative z-10 mb-10 xl:mb-0  h-full text-black">
                     <div className='grid grid-cols-5 place-items-center gap-2 
-                    h-[70px] mt-15 xl:mt-20 bg-white xl:w-[35%] ml-2 mr-2 xl:mr-2 xl:ml-10 rounded'>
+                    h-[70px] mt-15 xl:mt-20 bg-white xl:w-[35%] md:w-[60%] ml-2 mr-2 xl:mr-2 xl:ml-10 rounded'>
                         <img src={bannerPhoto.src} alt="bannerPhoto" className='col-span-1 h-15 ml-2 xl:ml-0 rounded xl:rounded-0 xl:w-[780px] xl:h-[70px]' />
                         <p className='flex place-items-center col-span-3 uppercase text-[12px]'>
                             Introducing our popular safari packages
@@ -36,16 +39,16 @@ function Herosection({ scrollToAbout }: HeroProps) {
                         </div>
                     </div>
                     <div className='mt-[80%] xl:mt-[20%] xl:mb-12 flex justify-end'>
-                        <div className=' w-[50%] xl:w-[15%] mr-10'>
-                            <p className='mb-5 text-white text-[14px] xl:text-[18px]'>
+                        <div className=' w-[50%] md:w-[30%] xl:w-[15%] mr-10'>
+                            <p className='mb-5 text-white text-[18px]'>
                                 Explore, discover, and unwind in Africa
                             </p>
                             <Link href={'/safaris'}>
                                 <button
                                     className='bg-[#FD6D0D] text-white cursor-pointer
-                                 h-[50px] lg:h-[70px] w-full mr-10 uppercase text-[12px] lg:text-[14px] rounded
+                                 h-[50px] lg:h-[70px] w-full mr-10 uppercase text-[14px] lg:text-[14px] rounded
                                 '>
-                                    All Our Safari Packages
+                                    Explore Safari Packages
                                 </button>
                             </Link>
                         </div>
